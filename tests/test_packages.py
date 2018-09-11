d45e8212518267be9c608903af9601d5d757a3da
Carles Amigó <fr3nd@fr3nd.net>
2018-09-11 13:32:44 +0200

    first real tests

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -9,5 +9,23 @@ import yaml
 
 @pytest.mark.parametrize("package", glob.glob('packages/*.yaml'))
 def test_package(package):
-    # Dummy test for testing
-    assert True
+    with open(package, 'r') as stream:
+        package_info = yaml.load(stream)
+
+        # Package name should have name and be 8 characters or less
+        assert len(package_info['name']) <= 8
+
+        # Package should have version and be 16 characters or less
+        assert len(package_info['version']) <= 16
+
+        # Package should have release and it must be integer
+        assert isinstance(package_info['release'], int)
+
+        # Package should have summary and be 80 characters or less
+        assert len(package_info['version']) <= 80
+
+        # Package should have author and be 128 characters or less
+        assert len(package_info['author']) <= 128
+
+        # Package should have package_author and be 128 characters or less
+        assert len(package_info['package_author']) <= 128
