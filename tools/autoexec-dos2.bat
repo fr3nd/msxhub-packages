d4c6b0b9f3d4ee9a445e0269c09029c536e7869a
Willem <willemtsade.git.2022@distributedrebirth.love>
2023-04-24 00:37:45 +0200

    Moved version to top

--- tools/autoexec-dos2.bat
+++ tools/autoexec-dos2.bat
@@ -1,10 +1,9 @@
 rem Managed autoexec.bat for msxhub-packages
 mode 80
+ver
 echo Extending search path;
 PATH + A:\ A:\SOFAROM
 PATH
-echo MSXDOS Version;
-ver
 echo Enabling renderer;
 omsxctl set renderer SDL
 echo Enabling throttling;
